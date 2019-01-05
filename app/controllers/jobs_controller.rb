31b1fcf84e06ef80c90ec17bcf524f1020d4bc09
Jethro Williams <jethrowilliams4@hotmail.com>
2019-01-05 13:28:42 +0000

    Add the basic functionality of being able to create a job with a company_name

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,9 +1,22 @@
 class JobsController < ApplicationController
   def new
+    @job = Job.new
+  end
 
+  def create
+    @job = Job.new(job_params)
+    @job.user = current_user
+    @job.save
+    redirect_to jobs_path
   end
 
   def index
-
+    @jobs = Job.all
   end
+end
+
+private
+
+def job_params
+  params.require(:job).permit(:company_name)
 end
